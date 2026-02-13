fd81a3debfe7283fc6ca8a538bcbe629a0395b01
lengthmannsohn <lengthmannsohn@hotmail.com>
2026-02-13 20:27:34 +0100

    JavaScript T3 String Ejercicio 3 Completado

--- 3.JavaScript/3. String/Ejercicio-3/script.js
+++ 3.JavaScript/3. String/Ejercicio-3/script.js
@@ -1,13 +1,24 @@
+const texto = `Curso de JavaScript Intermedio. 
+Aprender JavaScript es muy divertido y la programación te ayuda a mejorar tus habilidades. 
+Además quiero recalcar que la programación tambien fomenta la creativida.`;
+
 function procesarTexto(texto) {
     if (texto.includes("JavaScript")) {
         console.log("Incluye JavaScript")
     }
-    else {console.log("No incluye JavaScript")};
+    else {console.log("No incluye JavaScript")}
+
+    const posicionNivel = texto.indexOf(" ", 9);
+    const finNivel = texto.indexOf(".", posicionNivel + 1);
+
+    const lenguaje = texto.slice(9,posicionNivel);
+    const nivel = texto.slice(posicionNivel + 1, finNivel);
+
+    const nuevoLenguaje = texto.replace(lenguaje, "TypeScript");
+    const nuevoTexto = nuevoLenguaje.replace(/la programación/g, "el desarrollo");
+
+return nuevoTexto
 };
 
-const texto = `Curso de JavaScript Intermedio. 
-Aprender JavaScript es muy divertido y la programación te ayuda a mejorar tus habilidades. 
-La programación tambien fomenta la creatividad`;
-
-nuevoTexto = procesarTexto(texto);
+const nuevoTexto = procesarTexto(texto);
 console.log(nuevoTexto);
