91892fe2ac45bf8b2ccf6dfcadb964b40dd741da
lengthmannsohn <lengthmannsohn@hotmail.com>
2026-02-14 17:35:08 +0100

    JavaScript T4 Number Ejercicio 1 A medias

--- 3.JavaScript/4. Number/Ejercicio-1/script.js
+++ 3.JavaScript/4. Number/Ejercicio-1/script.js
@@ -1,1 +1,9 @@
 const precioUnidad = 5;
+const unidades = 10;
+const precioTotal = precioUnidad * unidades;
+const descuento = 0.2;
+const descuentoCantidad = precioTotal * descuento;
+const precioConDescuento = precioTotal - descuentoCantidad;
+
+console.log(precioConDescuento);
+
