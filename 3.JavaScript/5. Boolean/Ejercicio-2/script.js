7801fbb78dc5883128e0ce7f65f6838a14c8f562
lengthmannsohn <lengthmannsohn@hotmail.com>
2026-05-05 09:40:14 +0200

    Javascript T5 Boolean Ejercicio 2 A medias

--- 3.JavaScript/5. Boolean/Ejercicio-2/script.js
+++ 3.JavaScript/5. Boolean/Ejercicio-2/script.js
@@ -4,3 +4,19 @@ const usuario = {
   hasPremium: false,
   canPost: true
 };
+
+function mostrarEstadoUsuario(){
+  if (usuario.isLoggedIn){
+    console.log("Si está logado")
+  } 
+  else {
+    console.log("No está logado")
+  }
+}
+
+const tareasCompletadas = [true, false, true, false];
+
+tareasCompletadas.forEach(element => {
+  console.log(`Tarea: ${element ? "completada" : "pendiente"}`)
+});
+
