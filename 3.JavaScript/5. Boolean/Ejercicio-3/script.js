6a23c6682ad1924fc98b016cfb34c1513fbcb891
lengthmannsohn <lengthmannsohn@hotmail.com>
2026-05-07 04:00:12 +0200

    Javascript T5 Boolean Ejercicio 3 A medias

--- 3.JavaScript/5. Boolean/Ejercicio-3/script.js
+++ 3.JavaScript/5. Boolean/Ejercicio-3/script.js
@@ -1,3 +1,20 @@
-const isReady;
+let isReady;
 
 console.log(isReady);
+console.log(typeof isReady);
+
+let isReadySafe = false;
+
+console.log(isReadySafe);
+console.log(typeof isReadySafe);
+
+if (isReady){
+    console.log("Está preparado")
+}
+else {
+    console.log("No está preparado")
+} // En este bloque se ejecuta el else porque aunque el valor no es false pero es falsy. Piensa que el condicional if igualmente
+// solo pregunta si isReady es true y en este caso no lo es, por lo que es normal que se ejecute el else
+
+let hasPermission;
+
